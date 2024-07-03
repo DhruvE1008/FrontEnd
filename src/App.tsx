1b78266e127b81d9bf90c9bb5e04bfc532c22cf8
Dhruv <deri361@aucklanduni.ac.nz>
2024-07-03 22:08:36 +1200

    Made a navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import Slideshow from './Embedded-Slideshow';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PageRouter from './Router';
+import NavBar from './NavigationBar';
+import './App.css';
 
 const App: React.FC = () => {
-  const slides = [
-    {
-      image: 'images/all .png',
-      text: 'All Options',
-    },
-    {
-      image: 'images/ice cream option.png',
-      text: 'Ice Cream Options',
-    },
-    {
-      image: 'images/popsicle.png',
-      text: 'Popsicle Options',
-    },
-    {
-      image: 'images/ice cream sandwich.png',
-      text: 'Ice Cream Sandwich Options',
-    },
-  ];
 
   return (
-    <div>
-      <div style={{textAlign: 'center'}}>
-        <h1>Explore our Range</h1>
+    <Router>
+      <div className ="App">
+        <header> 
+          <div className = "text-container">
+            <h1>Ice Cream Store</h1>
+          </div> 
+          <NavBar /> 
+        </header>
+        <main>
+          <PageRouter />
+        </main>
       </div>
-      <Slideshow slides ={slides} />
-    </div>
+    </Router>
   );
 };
 
