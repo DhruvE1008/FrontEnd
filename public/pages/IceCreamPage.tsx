129c84043b8d14bda0429785eae4571f49f907eb
Dhruv <deri361@aucklanduni.ac.nz>
2024-07-04 15:51:39 +1200

    Created Options for ice cream

--- public/pages/IceCreamPage.tsx
+++ public/pages/IceCreamPage.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Button, Grid, Box } from '@mui/material';
 
 const IceCreamPage: React.FC = () => {
-    return <div>Select Ice Cream Flavour</div>;
+    const buttons = [
+        { text: 'Vanilla', image: 'images/vanilla.png' },
+        { text: 'Chocolate', image: 'images/chocolate.png' },
+        { text: 'Strawberry', image: 'images/strawberry.png' },
+        { text: 'Hokey Pokey', image: 'images/hokey pokey.png' },
+        { text: 'Butterscotch', image: 'images/butterscotch.png' },
+        { text: 'Cookies and Cream', image: 'images/cookies and cream.png' },
+        { text: 'Bubblegum', image: 'images/Bubblegum.png' },
+        { text: 'Mint', image: 'images/mint.png' },
+        { text: 'Neapolitan', image: 'images/neapolitan.png' },
+      ];
+    return (
+        <div>
+            <h1>Select Ice Cream Flavour</h1>
+            <h2>There are 9 amazing flavours to choose from</h2>
+            <Grid container spacing={2} justifyContent="center">
+                {buttons.map((btn, index) => (
+                    <Grid item xs={4} key={index}>
+                        <Button
+                        variant="contained"
+                        fullWidth
+                        style={{ flexDirection: 'column', padding: '16px', zIndex: -10 }}
+                        >
+                            <img
+                            src={btn.image}
+                            alt={btn.text}
+                             style={{ width: '60px', height: '60px', marginBottom: '8px' }}
+                            />
+                            <Box>
+                                {btn.text}
+                            </Box>
+                        </Button>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
 };
 
 export default IceCreamPage;
